1cb8b68241aa1b8707b2cf896e599033a63a96b5
Lizza96 <ia-hugo@hotmail.com>
2026-01-08 16:24:05 +0100

    feature: number of selected seats are displayed in the summary below the seats selection

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const avaliableSeats = document.querySelectorAll(
   '.seat:not(.selected):not(.occupied)'
 );
 
+let selectedSeatsCount = 0;
+const countElement = document.querySelector('#count');
+
 //event listeners
 for (const seat of avaliableSeats) {
   seat.addEventListener('click', () => clickSeat(seat));
@@ -11,4 +14,12 @@ for (const seat of avaliableSeats) {
 //functions
 function clickSeat(seatElement) {
   seatElement.classList.toggle('selected');
+
+  if (seatElement.classList.contains('selected')) {
+    selectedSeatsCount += 1;
+  } else {
+    selectedSeatsCount -= 1;
+  }
+
+  countElement.innerText = selectedSeatsCount;
 }
