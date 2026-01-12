77224d8eb8d117c0995323c5d020bc870d861ff3
Lizza96 <ia-hugo@hotmail.com>
2026-01-12 16:02:44 +0100

    add empty function that will display all movies as select options

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,11 @@ const totalElement = document.querySelector('#total');
 
 const movieSelect = document.querySelector('#movie');
 
+displaySelectOptions();
+
+// functions
+async function displaySelectOptions() {}
+
 function displayTotalCost() {
   totalElement.innerText = selectedSeatsCount * Number(movieSelect.value);
 }
