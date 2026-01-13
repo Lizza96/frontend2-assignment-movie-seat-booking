b0226b9370e9af6bc02b246e534bf82e7aeb16cc
Lizza96 <ia-hugo@hotmail.com>
2026-01-13 14:48:07 +0100

    seperate seat logic into seat.js and DOM rendering logic into ui.js. Added new function displaySeatCount and added a parameter (seast count) to function displayTotalCost. displayMovieOptions is no longer async

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,24 @@
 import loadMovies from './movie.js';
+import { clickSeat } from './seat.js';
+import { displayMovieOptions, displayTotalCost } from './ui.js';
+import { selectedSeatsCount } from './seat.js';
 
 // variables
 const avaliableSeats = document.querySelectorAll(
   '.seat:not(.selected):not(.occupied)'
 );
 
-let selectedSeatsCount = 0;
-const countElement = document.querySelector('#count');
-
-const totalElement = document.querySelector('#total');
-
 const movieSelect = document.querySelector('#movie');
 
-await displayMovieOptions();
+const movies = await loadMovies();
 
-// functions
-async function displayMovieOptions() {
-  const movies = await loadMovies();
-
-  for (const movie of movies) {
-    const optionsElement = document.createElement('option');
-
-    optionsElement.innerText = `${movie.name} (${movie.price} kr)`;
-    optionsElement.value = movie.price;
-
-    movieSelect.append(optionsElement);
-  }
-}
-
-function displayTotalCost() {
-  totalElement.innerText = selectedSeatsCount * Number(movieSelect.value);
-}
-
-function clickSeat(seatElement) {
-  seatElement.classList.toggle('selected');
-
-  if (seatElement.classList.contains('selected')) {
-    selectedSeatsCount += 1;
-  } else {
-    selectedSeatsCount -= 1;
-  }
-
-  countElement.innerText = selectedSeatsCount;
-  displayTotalCost();
-}
+// Inital startup select options
+displayMovieOptions(movies);
 
 // event listeners
-movieSelect.addEventListener('change', displayTotalCost);
+movieSelect.addEventListener('change', () =>
+  displayTotalCost(selectedSeatsCount)
+);
 
 avaliableSeats.forEach((seat) => {
   seat.addEventListener('click', () => clickSeat(seat));
