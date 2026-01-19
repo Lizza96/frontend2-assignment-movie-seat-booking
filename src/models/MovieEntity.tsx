c58ec862a1689e2a8401d6e4aef9f5b666832fa2
Lizza96 <ia-hugo@hotmail.com>
2026-01-19 16:27:17 +0100

    added MovieEntity class

---
+++ src/models/MovieEntity.tsx
@@ -0,0 +1,12 @@
+class Movie {
+  title: string;
+  price: number;
+  year: number;
+
+  constructor(title: string, price: number, year: number) {
+    this.title = title;
+    this.price = price;
+    this.year = year;
+  }
+}
+export default Movie;
