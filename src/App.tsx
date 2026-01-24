c46c0a0b5609c3f82ddcce4a23aaa5c7cad7a67c
Lizza96 <ia-hugo@hotmail.com>
2026-01-24 13:13:46 +0100

    feat: added a useEffect that fetches the bookings once on mount and sets the bookings in a useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BookingSummary } from './components/BookingSummary';
 import { MoviePicker } from './components/MoviePicker';
 import { SeatLegend } from './components/SeatLegend';
 import { SeatGrid } from './components/SeatGrid';
+import loadBookings from './api/bookings';
+import type { SeatBookings } from './interfaces/SeatBookings';
 
 function App() {
   const [selectedSeatsCount, setSelectedSeatsCount] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [bookings, setbookings] = useState({} as SeatBookings);
+
+  //Fetch bookings from API, once on mount
+  useEffect(() => {
+    async function getBookingsData() {
+      const fetchedBookings = await loadBookings();
+      setbookings(fetchedBookings);
+    }
+
+    getBookingsData();
+  }, []);
 
   return (
     <>
