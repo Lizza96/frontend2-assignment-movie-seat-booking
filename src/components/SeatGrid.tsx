4184ed771f2b6c97417d13f9efb999ed9e9641ff
Lizza96 <ia-hugo@hotmail.com>
2026-01-24 16:46:32 +0100

    fix: removed redundant comments in SeatGrid.tsx.

--- src/components/SeatGrid.tsx
+++ src/components/SeatGrid.tsx
@@ -8,22 +8,11 @@ type SeatGridProps = {
 export const SeatGrid = ({ selectedMovieBookings }: SeatGridProps) => {
   return (
     <>
-      {/* Row A */}
       <SeatRow row="a" rowBookings={selectedMovieBookings['a']} />
-
-      {/* Row B */}
       <SeatRow row="b" rowBookings={selectedMovieBookings['b']} />
-
-      {/* Row C */}
       <SeatRow row="c" rowBookings={selectedMovieBookings['c']} />
-
-      {/* Row D */}
       <SeatRow row="d" rowBookings={selectedMovieBookings['d']} />
-
-      {/* Row E */}
       <SeatRow row="e" rowBookings={selectedMovieBookings['e']} />
-
-      {/* Row F */}
       <SeatRow row="f" rowBookings={selectedMovieBookings['f']} />
     </>
   );
