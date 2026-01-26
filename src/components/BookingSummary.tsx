9fb872795293e056bf8241537932ab91c4ffd6e3
Lizza96 <ia-hugo@hotmail.com>
2026-01-26 21:00:38 +0100

    feat: added button to book tickets

--- src/components/BookingSummary.tsx
+++ src/components/BookingSummary.tsx
@@ -8,9 +8,22 @@ export const BookingSummary = ({
   totalPrice,
 }: BookingSummaryProps) => {
   return (
-    <p className="text">
-      You have selected <span id="count">{selectedSeatsCount}</span> seats for a
-      price of $<span id="total">{totalPrice}</span>
-    </p>
+    <div className="booking-wrapper">
+      <p className="text">
+        You have selected <span id="count">{selectedSeatsCount}</span> seats for
+        a price of $<span id="total">{totalPrice}</span>
+      </p>
+      <button
+        disabled={selectedSeatsCount == 0 ? true : undefined}
+        className="book-tickets-btn"
+        title={
+          selectedSeatsCount == 0
+            ? 'Du måste välja platser innan du kan boka biljetter'
+            : 'Boka biljetterna'
+        }
+      >
+        Boka
+      </button>
+    </div>
   );
 };
