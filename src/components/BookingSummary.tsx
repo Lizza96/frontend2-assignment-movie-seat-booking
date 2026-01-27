9966e44677a826646921df8086afff61892d8f65
Lizza96 <ia-hugo@hotmail.com>
2026-01-27 10:05:41 +0100

    feature: booking form is shown when clicking 'Book Tickets' button

--- src/components/BookingSummary.tsx
+++ src/components/BookingSummary.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { BookingButton } from './BookingButton';
+
 type BookingSummaryProps = {
   selectedSeatsCount: number;
   totalPrice: number;
@@ -7,23 +10,60 @@ export const BookingSummary = ({
   selectedSeatsCount,
   totalPrice,
 }: BookingSummaryProps) => {
+  const [showForm, setShowForm] = useState(false);
+
+  function displayForm() {
+    setShowForm(true);
+  }
+
   return (
-    <div className="booking-wrapper">
-      <p className="text">
-        You have selected <span id="count">{selectedSeatsCount}</span> seats for
-        a price of $<span id="total">{totalPrice}</span>
-      </p>
-      <button
-        disabled={selectedSeatsCount == 0 ? true : undefined}
-        className="book-tickets-btn"
-        title={
-          selectedSeatsCount == 0
-            ? 'Du måste välja platser innan du kan boka biljetter'
-            : 'Boka biljetterna'
-        }
-      >
-        Boka
-      </button>
-    </div>
+    <>
+      <div className="booking-wrapper">
+        <p className="text">
+          You have selected <span id="count">{selectedSeatsCount}</span> seats
+          for a price of $<span id="total">{totalPrice}</span>
+        </p>
+
+        {!showForm ? (
+          <button
+            onClick={selectedSeatsCount <= 0 ? undefined : displayForm}
+            disabled={selectedSeatsCount <= 0 ? true : undefined}
+            className="book-tickets-btn"
+            title={
+              selectedSeatsCount <= 0
+                ? 'You must select seats before you can book tickets'
+                : 'Book Tickets'
+            }
+          >
+            Book Tickets
+          </button>
+        ) : null}
+      </div>
+
+      {showForm ? (
+        <form>
+          <div className="form-row">
+            <label>Name:</label>
+            <input />
+          </div>
+          <div className="form-row">
+            <label>Phone number:</label>
+            <input />
+          </div>
+          <button
+            onClick={selectedSeatsCount <= 0 ? undefined : displayForm}
+            disabled={selectedSeatsCount <= 0 ? true : undefined}
+            className="book-tickets-btn"
+            title={
+              selectedSeatsCount <= 0
+                ? 'You must select seats before you can book tickets'
+                : 'Book Tickets'
+            }
+          >
+            Book {selectedSeatsCount} ticket(s)
+          </button>
+        </form>
+      ) : null}
+    </>
   );
 };
